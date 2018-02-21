da95c50bb14ae9a0c57b5d6a6bf24aeedc0bca7f
Aditya Sridhar <as734@duke.edu>
2018-02-21 10:31:48 -0500

    Started front-end, added classes, and created Turtle class

---
+++ src/windows/UserHistoryWindow.java
@@ -0,0 +1,13 @@
+package windows;
+
+import javafx.scene.Group;
+
+public class UserHistoryWindow implements WindowElements {
+
+	private Group root;
+	
+	public UserHistoryWindow(Group root) {
+		this.root = root;
+	}
+	
+}
