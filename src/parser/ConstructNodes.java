7b1f67750ae9d47ba4914a0739413f9f1e9b64fe
Summer Smith <sas118@duke.edu>
2018-02-24 16:57:54 -0500

    parser files made

---
+++ src/parser/ConstructNodes.java
@@ -0,0 +1,14 @@
+package parser;
+
+import java.util.List;
+import nodes.Node;
+
+public class ConstructNodes {
+
+	List<String> input;
+	List<Node> nodes;
+	
+	public ConstructNodes(List<String> strings) {
+		input = strings;
+	}
+}
