862d14b7d59be459d1d9bf3b3baafee76fad8b6c
Aditya Sridhar <as734@duke.edu>
2018-02-27 01:14:49 -0500

    Progress with Front-End

--- src/gui_elements/labels/BackgroundColorLabel.java
+++ src/gui_elements/labels/BackgroundColorLabel.java
@@ -1,5 +1,13 @@
 package gui_elements.labels;
 
-public class BackgroundColorLabel extends Labels {
+import javafx.scene.Group;
+import javafx.scene.control.Label;
 
+public class BackgroundColorLabel extends DefaultLabel {
+
+	private static final String PROPERTIES_FILENAME = "background_color_label.properties";	
+	
+	public BackgroundColorLabel(Label label, Group root) {
+		super(label, root, PROPERTIES_FILENAME);
+	}
 }
--- src/windows/Windows.java
+++ src/windows/Windows.java
@@ -1,5 +1,26 @@
 package windows;
 
-public interface WindowElements {
+import java.io.InputStream;
+import java.util.Properties;
 
+import javafx.scene.Parent;
+
+public abstract class Windows {
+
+	protected final String X_LOC_STRING = "x_loc";
+	protected final String Y_LOC_STRING = "y_loc";
+	protected final String WIDTH = "width";
+	protected final String HEIGHT = "height";
+	protected final String DIRECTORY_STRING = "data/windows_properties/";
+	protected int x, y, width, height;
+	protected Properties properties;
+	protected InputStream input;	
+		
+	protected abstract void getProperties();
+	protected abstract void setX(int x);
+	protected abstract void setY(int y);
+	protected abstract int getX();
+	protected abstract int getY();
+	protected abstract void createWindow();
+	public abstract Parent getWindowArea();
 }
