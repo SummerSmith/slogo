b5c63d764032da769f76eeb8433a25957869fe5d
Maddie Wilkinson <mw350@duke.edu>
2018-02-26 19:36:41 -0500

    started evaluate methods in Nodes

--- src/nodes/CommandNode.java
+++ src/nodes/CommandNode.java
@@ -1,17 +1,30 @@
 package nodes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Executable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 import turtle.Turtle;
 
 public class CommandNode extends Node {
 	
 	public CommandNode(String name, Turtle t) {
 		super(name, t);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
 	public double evaluate() {
-		// TODO Auto-generated method stub
+		List<Double> args = new ArrayList<>();
+		for(Node n : myChildren) {
+			args.add(n.evaluate());
+		}
+		
 		return 0;
 	}
 
---
+++ src/nodes/UserCommandNode.java
@@ -0,0 +1,18 @@
+package nodes;
+
+import turtle.Turtle;
+
+public class UserCommandNode extends CommandNode {
+	
+	public UserCommandNode(String name, Turtle t) {
+		super(name, t);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double evaluate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
