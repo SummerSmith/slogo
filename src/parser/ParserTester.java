317a1b3a3ea1f0b8195372aea4fcd9fcdf684b4c
Maddie Wilkinson <mw350@duke.edu>
2018-02-27 23:34:46 -0500

    save ParserTester edits

--- src/parser/ParserTester.java
+++ src/parser/ParserTester.java
@@ -9,19 +9,11 @@ public class ParserTester {
 
 	public static void main(String args[]) throws Exception {
 		//String toProcess = "+ fd 50 rt 90 fd fd 50";
-		String toProcess = "fd 50 rt 90 fd 50";
+		String toProcess = "fd sum 40 cos 0";
 		ProcessString process = new ProcessString();
 		ArrayList<String> command = (ArrayList<String>) process.processString(toProcess);
 		Turtle t = new Turtle(new Point(0,0));
 		System.out.println("Point Before: " + t.getLayoutX() + ", " + t.getLayoutY());
-		//command.add("+");
-		//command.add("fd");
-		//command.add("50");
-//		command.add("rt");
-//		command.add("90");
-//		command.add("fd");
-//		command.add("fd");
-//		command.add("20");
 		new ConstructNodes(t, command, "English");
 //		System.out.println("Point After: " + t.getXLocation() + ", " + t.getYLocation());
 		System.out.println("Point After: " + t.getLocation());
