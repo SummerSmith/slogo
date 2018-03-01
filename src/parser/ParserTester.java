768bf002d5f3efdd74771cc2063115edd7dd35c1
Maddie Wilkinson <mw350@duke.edu>
2018-02-28 22:41:06 -0500

    changed Point class

--- src/parser/ParserTester.java
+++ src/parser/ParserTester.java
@@ -1,6 +1,5 @@
 package parser;
 
-import java.awt.Point;
 import java.util.ArrayList;
 
 import turtle.Turtle;
@@ -9,7 +8,15 @@ public class ParserTester {
 
 	public static void main(String args[]) throws Exception {
 //		String toProcess = "+ fd 50 rt 90 fd fd 50";
-		String toProcess = "repeat fd 5 [ fd 5 ]";
+//		String toProcess = "repeat fd 5 [ fd 5 ]";
+		String toProcess = "repeat 18 [\n" + 
+				"  forward 50\n" + 
+				"  right 150\n" + 
+				"  forward 60                       \n" + 
+				"  right 100\n" + 
+				"  forward 30\n" + 
+				"  right 90\n" + 
+				"]";
 		ProcessString process = new ProcessString();
 		ArrayList<String> command = (ArrayList<String>) process.processString(toProcess);
 		System.out.println(command);
