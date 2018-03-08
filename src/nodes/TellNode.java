91012b2acc6f750b82f17f0e6f33404702bdbbc0
Maddie Wilkinson <mw350@duke.edu>
2018-03-08 16:25:02 -0500

    fixed tellnode

--- src/nodes/TellNode.java
+++ src/nodes/TellNode.java
@@ -1,13 +1,20 @@
 package nodes;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import error.Error;
 import parser.TurtleManager;
+import slogo_team12.Display;
+import turtle.CreateTurtle;
+import turtle.Turtle;
 
 public class TellNode extends Node{
 
-	public TellNode(String name) {
-		super(name);
+	public TellNode(String s) {
+		super(s);
 	}
+	
 	public TellNode(Node curr) {
 		super(curr);
 	}
@@ -21,14 +28,22 @@ public class TellNode extends Node{
 				System.out.println("Unknown Commands catches!");
 				Exception e_0 = new Exception("Unknown Commands");
 				new Error(e_0);
-				//e.printStackTrace();
 			}
 			else {
-				TurtleManager.addActiveTurtle(curr.getType());
+				ArrayList<Turtle> allTurtles = (ArrayList<Turtle>) TurtleManager.getAllTurtles();
+				HashMap<Integer, Turtle> allTurtlesByID = (HashMap<Integer, Turtle>) TurtleManager.getAllTurtlesByID();
 				value = Double.parseDouble(curr.getType());
+				System.out.println(allTurtles.size());
+				if(allTurtles.contains(allTurtlesByID.get((int) value))) {
+					TurtleManager.addActiveTurtle(curr.getType());
+					System.out.println("Existing turtle");
+				}
+				else {
+					new CreateTurtle(Display.getRoot());
+					System.out.println("Created new turtle");
+				}
 			}
 		}
 		return value;
 	}
-
 }
