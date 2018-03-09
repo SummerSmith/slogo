47163b3e6a5751878fb309383b7bf25897ad7e18
yl366 <yl366@duke.edu>
2018-03-09 17:42:47 -0500

    solve all errors

--- src/Test/ParserTester.java
+++ src/Test/ParserTester.java
@@ -12,15 +12,15 @@ public class ParserTester {
 	public static void main(String args[]) throws Exception {
 //		String toProcess = "+ fd 50 rt 90 fd fd 50";
 //		String toProcess = "tell [ 1 2 3 ]";
-		List<Integer> turts = new ArrayList<>();
-		turts.add(1);
+		List<Double> turts = new ArrayList<>();
+		turts.add(1.0);
 //		turts.add(2);
 		TurtleManager.setActiveTurtles(turts);
 //		TurtleManager.addTurtle(new Turtle());
 		System.out.println(TurtleManager.getActiveTurtles());
 		System.out.println(TurtleManager.getAllTurtles());
-		BackEndManager backend = new BackEndManager(toProcess, "English");
-		backend.parse();
+		//BackEndManager backend = new BackEndManager(toProcess, "English");
+		//backend.parse();
 //		ProcessString process = new ProcessString();
 //		ArrayList<String> command = (ArrayList<String>) process.processString(toProcess);
 //		System.out.println(command);
